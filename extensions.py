8c5bb8954c7dded2342be049055f449e61ec4604
Knocktern <aasakib13@gmail.com>
2026-03-06 22:27:11 +0600

    Switch from eventlet to gevent for Python 3.14 compatibility

--- extensions.py
+++ extensions.py
@@ -4,4 +4,4 @@ from flask_socketio import SocketIO
 
 db = SQLAlchemy()
 mail = Mail()
-socketio = SocketIO(cors_allowed_origins="*")
+socketio = SocketIO(cors_allowed_origins="*", async_mode='gevent')
